6ed471e08fd10648975334a9ab4c03c285d73e23
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-18 17:30:01 +0200

    landing page: fix combee position

--- src/components/threejs/combee/Combee.tsx
+++ src/components/threejs/combee/Combee.tsx
@@ -4,20 +4,13 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const BeeModel = () => {
     const model = useLoader(GLTFLoader, '/assets/combee.glb')
-    return (
-        <primitive
-            object={model.scene}
-            // scale={1.25}
-            rotation={[0, -1.1, 0.25]}
-            position={[1.5, -3.5, 0]}
-        />
-    )
+    return <primitive object={model.scene} rotation={[0, -1.1, 0.25]} position={[1.5, -3.5, 0]} />
 }
 
 const Combee = () => {
     return (
         <Canvas>
-            <directionalLight castShadow position={[0, 0, 2]} intensity={0.4} />
+            <directionalLight castShadow position={[3, 0, 2]} intensity={0.4} />
             <ambientLight intensity={0.2} />
             <Suspense fallback={null}>
                 <BeeModel />
