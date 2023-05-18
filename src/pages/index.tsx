1dc38d64f41d156e95c4b658d400796293993b26
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-18 12:44:04 +0200

    landing page: add titles and beehive illustrations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { BeehiveSVG } from '@/components/BeehiveSVG'
 import { StyledLandingPage } from '@/styles/pages/StyledLandingPage'
 
 const LandingPage = () => {
     return (
         <StyledLandingPage>
-            <h1>Maïa Da Silva</h1>
-            <h2>Front-end Developer</h2>
+            <div>
+                <h1>
+                    Maïa
+                    <br /> Da Silva
+                </h1>
+                <h2>Front-end Developer</h2>
+            </div>
+            <div>
+                <BeehiveSVG />
+                <BeehiveSVG />
+            </div>
         </StyledLandingPage>
     )
 }
--- src/styles/pages/StyledLandingPage.ts
+++ src/styles/pages/StyledLandingPage.ts
@@ -1,5 +1,49 @@
 import styled from 'styled-components'
+import { rem } from '../utils/utils'
 
 export const StyledLandingPage = styled.main`
     padding: ${({ theme }) => theme.spacing.xl};
+    height: 100vh;
+
+    > div {
+        width: fit-content;
+        h1 {
+            font-size: ${rem(125)};
+            letter-spacing: ${rem(-4)};
+            line-height: ${rem(90)};
+            font-variant-caps: normal;
+        }
+
+        h2 {
+            padding-left: ${rem(5)};
+            color: ${({ theme }) => theme.colors.primary};
+            font-size: ${({ theme }) => theme.fontSizes.h1};
+            letter-spacing: ${rem(13.5)};
+            text-align: justify;
+            text-justify: inter-character;
+        }
+    }
+
+    svg {
+        position: absolute;
+
+        &:first-of-type {
+            top: ${rem(-50)};
+            right: ${rem(-50)};
+        }
+
+        &:last-of-type {
+            transform: rotate(90);
+            bottom: ${rem(-50)};
+            left: ${rem(-50)};
+        }
+
+        path:nth-child(odd) {
+            fill: ${({ theme }) => theme.colors.secondary};
+        }
+
+        path:nth-child(even) {
+            fill: ${({ theme }) => theme.colors.primary};
+        }
+    }
 `
