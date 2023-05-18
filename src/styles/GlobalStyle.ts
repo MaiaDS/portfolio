68ffb2bc17582b4506b41da76316ddf533240c5d
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-05-18 11:26:52 +0200

    style: add global style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,5 +2,51 @@ import { createGlobalStyle } from 'styled-components'
 import { ICustomTheme } from './utils/theme'
 
 export const GlobalStyle = createGlobalStyle<{ theme: ICustomTheme }>`
-    
+    width: 100vw;
+    min-height: 100vh;
+    overflow: hidden;
+
+    background-color: ${({ theme }) => theme.colors.light};
+    color: ${({ theme }) => theme.colors.dark};
+    font-size: ${({ theme }) => theme.fontSizes.default};
+
+    * {
+        box-sizing: border-box;
+    }
+
+    button {
+        appearance: none;
+        background-color: transparent;
+        cursor: pointer;
+    }
+
+    a {
+        cursor: pointer;
+        display: block;
+    }
+
+    h1,
+    h2,
+    h3,
+    .title {
+        text-transform: uppercase;
+        font-variant-caps: all-small-caps;
+        letter-spacing: 0.25rem;
+    }
+
+    h1 {
+        font-size: ${(props) => props.theme.fontSizes.h1};
+    }
+
+    h2 {
+        font-size: ${(props) => props.theme.fontSizes.h2};
+    }
+
+    h3 {
+        font-size: ${(props) => props.theme.fontSizes.h3};
+    }
+
+    h4 {
+        font-size: ${(props) => props.theme.fontSizes.h4};
+    }
 `
