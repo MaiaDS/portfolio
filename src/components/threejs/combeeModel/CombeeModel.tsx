cdb591b42e4c03fab74259e0416b2d834152827f
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-06-08 16:39:32 +0200

    landing page: fix combee position according to screen width

--- src/components/threejs/combeeModel/CombeeModel.tsx
+++ src/components/threejs/combeeModel/CombeeModel.tsx
@@ -5,13 +5,33 @@ import { colors } from '@/styles/utils/utils'
 import { beeStripesShader } from '../shaders/beeShader'
 import { Float } from '@react-three/drei'
 import { ScreenFormats, useCustomThemeContext } from '@/utils/CustomTheme'
+import { useState, useEffect } from 'react'
 
 const CombeeModel = () => {
     const model: any = useLoader(GLTFLoader, '/assets/combee.glb')
     const scene = model.scene
     scene.children = scene.children.filter((child: any) => child.name !== 'Tail')
     const { theme } = useCustomThemeContext()
-    const positionX = theme !== ScreenFormats.DESKTOP ? 1 : 4.5
+    const [containerWidth, setContainerWidth] = useState<number>()
+    const [positionX, setPositionX] = useState<number>(1)
+
+    useEffect(() => {
+        setContainerWidth(window.innerWidth)
+        window.addEventListener('resize', () => {
+            setContainerWidth(window.innerWidth)
+        })
+    }, [])
+
+    useEffect(() => {
+        if (containerWidth) {
+            if (theme === ScreenFormats.DESKTOP) {
+                setPositionX((containerWidth * 4.5) / 1440)
+            } else {
+                setPositionX(1)
+            }
+        }
+    }, [containerWidth])
+
     return (
         <Float rotationIntensity={0.1} speed={4}>
             <group
